85b6c8c0f364712b9a6454135f79e19a52758115
ji-park <qkrskdjf@naver.com>
2021-11-09 20:30:38 +0900

    Refactoring : 언어별 검색 기능 수정

--- repository/autocomplete_respository.py
+++ repository/autocomplete_respository.py
@@ -2,6 +2,8 @@ from model.models import Company, CompanyName, TagName
 from app import db
 
 def findCompanyName(query, language):
-    results = CompanyName.query.filter(CompanyName.name.like('%'+ query +'%')).all()
+    results = CompanyName.query.filter(CompanyName.name.like('%'+ query +'%'), \
+    CompanyName.type == language).all()
+    # results.filter(CompanyName.type == language).all()
     return results
 
