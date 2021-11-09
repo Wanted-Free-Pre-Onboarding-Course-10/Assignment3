7a45aed871b51d6368f3c25c9a73d06efa09521a
godtaehee <godtaeheeDev@gmail.com>
2021-11-10 00:08:59 +0900

    Revert "Feature/duplicate defined schema"

--- repository/autocomplete_respository.py
+++
@@ -1,9 +0,0 @@
-from model.models import Company, CompanyName, TagName
-from app import db
-
-def findCompanyName(query, language):
-    results = CompanyName.query.filter(CompanyName.name.like('%'+ query +'%'), \
-    CompanyName.type == language).all()
-    # results.filter(CompanyName.type == language).all()
-    return results
-
--- serializers/company.py
+++
@@ -1,7 +0,0 @@
-# from marshmallow import Schema, fields
-# from model.models import Company, CompanyName
-#
-# class CreateCompanyRequestScheme(Schema):
-#
-# class CompanyRequestName(Schema):
-#
